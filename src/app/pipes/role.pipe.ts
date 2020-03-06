3d951c1583c8ed4000ea007b9dd18b9c291084df
Milana <milana.bijeljanin@gmail.com>
2020-03-06 14:59:36 +0100

    rolePipe

---
+++ src/app/pipes/role.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Roles } from "../models/system.models";
+
+@Pipe({
+  name: 'role'
+})
+export class RolePipe implements PipeTransform {
+
+  transform(role: string): string {
+    switch (role) {
+      case Roles.doctor:
+        return 'Dr.';
+      case Roles.nurse:
+        return 'Nr.';
+    }
+  }
+}
