42a169a363695489f9a63c177d629c8eb21b6a17
Kristina <ilic.kristina1996@gmail.com>
2020-03-06 10:22:00 +0100

    Upcoming appointments

--- src/app/components/upcoming-appointments/upcoming-appointments.component.ts
+++ src/app/components/upcoming-appointments/upcoming-appointments.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class UpcomingAppointmentsComponent implements OnInit {
   @Input() numberOfUpcomingAppointments = 2;
   @Input() date = "Aug 30";
-  @Input() dateDifference = "Two days ago";
+  @Input() dateDifference = "2 days ago";
   @Input() appointment = "New Patient Appointment";
   @Input() provider = "Dr. Emily Jonson";
 
