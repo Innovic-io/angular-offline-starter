4919090aecdb36e6c24d0caa953bcf3a70c300de
Milana <milana.bijeljanin@gmail.com>
2020-03-06 11:37:02 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import {QuickLinksComponent} from "./components/quick-links/quick-links.component";
-import {WelcomeComponent} from "./components/welcome/welcome.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { QuickLinksComponent } from "./components/quick-links/quick-links.component";
+import { WelcomeComponent } from "./components/welcome/welcome.component";
 import { AppointmentsComponent } from './pages/appointments/appointments.component';
 import { UpcomingAppointmentsComponent } from './components/upcoming-appointments/upcoming-appointments.component';
 import { CreateComponent } from './pages/appointments/create/create.component';
@@ -18,11 +21,13 @@ import { CreateComponent } from './pages/appointments/create/create.component';
     QuickLinksComponent,
     AppointmentsComponent,
     UpcomingAppointmentsComponent,
-    CreateComponent
+    CreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
