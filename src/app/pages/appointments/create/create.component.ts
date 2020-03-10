b54c3bfec89e5dc24f07cf00a2ca4436d0bf77e1
Vlado <ivanafustar@hotmail.com>
2020-03-10 16:29:23 +0100

    Task #10

--- src/app/pages/appointments/create/create.component.ts
+++ src/app/pages/appointments/create/create.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 import { AppointmentModel } from '../../../models/appointment.model';
 import { AppointmentService } from '../../../services/appointment.service';
 
-import { doctor} from '../../../data/dummy';
+import { doctor } from '../../../data/dummy';
 import { AppointmentType, enumSelector } from '../../../models/system.models';
 
 @Component({
@@ -14,25 +14,17 @@ import { AppointmentType, enumSelector } from '../../../models/system.models';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
-  constructor(private appointmentService: AppointmentService, private router: Router) {
-  }
-
   providers = [doctor];
   appointment = new AppointmentModel();
   appTypes = enumSelector(AppointmentType);
   alert: boolean;
 
+  constructor(private appointmentService: AppointmentService, private router: Router) {
+  }
+
   checkAvailableAppointment() {
     const arrayOfAppointments = this.appointmentService.getAllDoctorAppointments(this.appointment.provider.guid);
-    const emptyArray = [];
 
-/*    arrayOfAppointments.forEach(appointment => {
-      if (this.appointment.date.getTime() === appointment.date.getTime()) {
-        emptyArray.push(appointment);
-      }
-    });
-    return emptyArray.length === 1;*/
     return arrayOfAppointments.some(appointment => this.appointment.date.getTime() === appointment.date.getTime());
   }
 
@@ -42,13 +34,12 @@ export class CreateComponent implements OnInit {
       if (this.alert) {
         return;
       }
-      this.alert = false;
+
       this.appointmentService.createAppointment(this.appointment);
       this.appointment = new AppointmentModel();
       alert('Appointment is created successfully!');
       form.reset();
     }
-    form.reset();
   }
 
   cancel() {
