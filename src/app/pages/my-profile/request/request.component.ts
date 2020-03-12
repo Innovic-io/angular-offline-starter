121be89e4122bb3f0dafc8674677e6cb3dceaf8e
Milana <milana.bijeljanin@gmail.com>
2020-03-12 16:42:25 +0100

    request

--- src/app/pages/my-profile/request/request.component.ts
+++ src/app/pages/my-profile/request/request.component.ts
@@ -1,7 +1,4 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { EmployeeModel } from '../../../models/employee.model';
-import { EmployeeService } from '../../../services/employee.service';
-
 
 @Component({
   selector: 'app-request',
@@ -10,27 +7,22 @@ import { EmployeeService } from '../../../services/employee.service';
 })
 export class RequestComponent implements OnInit {
   @Output() resultInput = new EventEmitter<number>();
-  user: EmployeeModel;
   firstNumber =  Math.floor(Math.random() * Math.floor(10));
   secondNumber = Math.floor(Math.random() * Math.floor(10));
-  math = this.firstNumber.toString() + ' + ' + this.secondNumber.toString();
-  result = this.firstNumber + this.secondNumber;
   submitDisabled = true;
 
-constructor(private employeeService: EmployeeService) { }
+  form = {
+    email: null
+  };
+
+constructor() { }
 
   ngOnInit(): void {
-  this.user = this.employeeService.getLoggedEmployee();
   }
 
   onChange(event) {
     // tslint:disable-next-line:radix
     const eventResult: number = parseInt(event.target.value);
-    if (this.result === eventResult) {
-      this.submitDisabled = false;
-    }
-    if (this.result !== eventResult) {
-      this.submitDisabled = true;
-    }
+    this.submitDisabled = (this.firstNumber + this.secondNumber) !== eventResult;
   }
 }
